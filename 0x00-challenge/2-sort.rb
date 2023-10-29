29e539b9f73791ffd693194bdcfeb3e6c1e8258f
larsen_dev <53650985+larsensong@users.noreply.github.com>
2023-10-29 21:22:07 +0300

    Update 2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -9,7 +9,7 @@ ARGV.each do |arg|
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
+    # convert to integer.
     i_arg = arg.to_i
 
     # insert result at the right position
